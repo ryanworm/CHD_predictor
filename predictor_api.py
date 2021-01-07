203561feb8ac1d037acde7fac741f75de40d14c1
Ryan Worm <ryan_worm@hotmail.com>
2021-01-07 14:21:29 -0500

    replaced with new page

--- predictor_api.py
+++ predictor_api.py
@@ -3,7 +3,7 @@ import joblib
 # create function under predictor_api
 def predict_outcome(form_data):
     # C:\Users\Ryan\Desktop\final_project_new\CHD_predictor\web_pg\static\models
-    predictor = joblib.load('./static/models/svc_model.sav')
+    predictor = joblib.load('./static/models/knn_model.sav')
     output = predictor.predict(form_data)
 
     print(output)
